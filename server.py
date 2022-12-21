53bb4c3797f88562973f45909417a586c77d34c6
PyTsun <53323309+PyTsun@users.noreply.github.com>
2022-12-21 10:05:04 +0800

    Added Invalid Subdomain Handler

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from httpx import get
+from httpx import get, ConnectError
 from fastapi import FastAPI
 from requests.exceptions import JSONDecodeError as JDE
 app = FastAPI()
@@ -11,3 +11,4 @@ async def proxyRequest(subdomain: str = None, path: str = None):
     if subdomain == None: return {"success": False, "message": "SimpleLegoProxy - Subdomain is a Required Query Argument that is missing."}
     try: return get(f'https://{subdomain}.roblox.com/{path}').json()
     except JDE: return {"success": False, "message": "SimpleLegoProxy - Site did not return JSON Data."}
+    except ConnectError: return {"success": False, "message": "SimpleLegoProxy - Queried Subdomain does not exist."}
