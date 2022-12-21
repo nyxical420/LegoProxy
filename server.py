d1488009eb152329e4429898b91115f1623fbfcc
PyTsun <53323309+PyTsun@users.noreply.github.com>
2022-12-21 13:36:45 +0800

    Added repo redirect and changed stuff

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from requests import get, JSONDecodeError
 
 app = FastAPI(
@@ -9,12 +10,16 @@ app = FastAPI(
     redoc_url=None
 )
 
-@app.get("/{subdomain}/{path}")
+@app.get("/")
+async def legoproxyHome():
+    return RedirectResponse("https://github.com/PyTsun/SimpleLegoProxy")
+
+@app.get("/{subdomain}")
 async def proxyRequest(subdomain: str = None, path: str = None):
     path = path.replace(".", "/")
-    if subdomain == None and path == None: return {"success": True, "message": "SimpleLegoProxy is Running!"}
+    if path == None: return {"success": False, "message": "SimpleLegoProxy - API Path is a Required Path Argument that is missing."}
+    if subdomain == None: return {"success": False, "message": "SimpleLegoProxy - Subdomain is a Required Path Argument that is missing."}
+    if subdomain == None and path == None: return {"success": False, "message": "SimpleLegoProxy - Subdomain and API Path is a Required Path Argument that is missing."}
 
-    if path == None: return {"success": False, "message": "SimpleLegoProxy - API Path is a Required Query Argument that is missing."}
-    if subdomain == None: return {"success": False, "message": "SimpleLegoProxy - Subdomain is a Required Query Argument that is missing."}
     try: return get(f'https://{subdomain}.roblox.com/{path}').json()
     except JSONDecodeError: return {"success": False, "message": "SimpleLegoProxy - Site did not return JSON Data."}
