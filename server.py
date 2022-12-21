f3d1a6e438c85f493196c010b4aae3256a192e66
PyTsun <53323309+PyTsun@users.noreply.github.com>
2022-12-21 16:33:30 +0800

    Update server.py

--- server.py
+++ server.py
@@ -18,17 +18,19 @@ async def legoproxyHome():
 
 @app.get("/{subdomain}", description="Non-Rotating Proxy Request")
 async def proxyRequest(subdomain: str, path: str):
-    path = path.replace(".", "/")
+    if path == None: return {"success": False, "message": "RoPortal - Path is a required Query Argument that is missing."}
 
     try: return get(f'https://{subdomain}.roblox.com/{path}').json()
     except JSONDecodeError: return {"success": False, "message": "RoPortal - Roblox API did not return JSON Data."}
 
 @app.get("/rotate/{subdomain}", description="Rotating Proxy Request")
 async def proxyRequest(subdomain: str, path: str):
-    path = path.replace(".", "/")
+    if subdomain == None: return {"success": False, "message": "RoPortal - Subdomain is a required Path Argument that is missing."}
+    if path == None: return {"success": False, "message": "RoPortal - Path is a required Query Argument that is missing."}
     
     if proxylist == []: return {"success": False, "message": "RoPortal - Proxy IP List is Empty. Please add IPs and Restart RoPortal."}
     proxy = choice(proxylist)
-    try: return get(f'https://{subdomain}.roblox.com/{path}', proxies={"https": f"https://{proxy}"}).json()
+    try: return get(f'https://{subdomain}.roblox.com/{path}', proxies={"http": f"http://{proxy}"}).json()
     except JSONDecodeError: return {"success": False, "message": "RoPortal - Roblox API did not return JSON Data."}
     except ConnectTimeout: return {"success": False, "message": f"RoPortal - Proxy Timed Out. Proxy IP: {proxy}"}
+    except : return {"success": False, "message": f"RoPortal - Proxy Timed Out. Proxy IP: {proxy}"}
