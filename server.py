9231fb3c1ee7d9d51f298c65e1d74692648107af
PyTsun <53323309+PyTsun@users.noreply.github.com>
2022-12-21 11:12:24 +0800

    Removed httpx

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from httpx import get, ConnectError
 from fastapi import FastAPI
-from requests.exceptions import JSONDecodeError as JDE
+from requests import get, JSONDecodeError
+
 app = FastAPI()
 
 @app.get("/")
@@ -10,5 +10,4 @@ async def proxyRequest(subdomain: str = None, path: str = None):
     if path == None: return {"success": False, "message": "SimpleLegoProxy - API Path is a Required Query Argument that is missing."}
     if subdomain == None: return {"success": False, "message": "SimpleLegoProxy - Subdomain is a Required Query Argument that is missing."}
     try: return get(f'https://{subdomain}.roblox.com/{path}').json()
-    except JDE: return {"success": False, "message": "SimpleLegoProxy - Site did not return JSON Data."}
-    except ConnectError: return {"success": False, "message": "SimpleLegoProxy - Queried Subdomain does not exist."}
+    except JSONDecodeError: return {"success": False, "message": "SimpleLegoProxy - Site did not return JSON Data."}
