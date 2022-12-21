ca6d1d047ccc9ee0463e6de34a12df1b59531f64
PyTsun <53323309+PyTsun@users.noreply.github.com>
2022-12-21 17:35:41 +0800

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from random import choice
 from fastapi import FastAPI
 from requests import get, JSONDecodeError, ConnectTimeout
 
+
 app = FastAPI(
     title="LegoProxy",
     description="A very simple rotating Roblox API Proxy",
@@ -17,19 +18,19 @@ async def legoproxyHome():
     return {"success": True, "message": "LegoProxy is Running!"}
 
 @app.get("/{subdomain}", description="Non-Rotating Proxy Request")
-async def proxyRequest(subdomain: str, path: str):
-    if path == None: return {"success": False, "message": "LegoProxy - Path is a required Query Argument that is missing."}
+async def proxyRequest(subdomain: str, e: str, password: str = None):
+    if e == None: return {"success": False, "message": "LegoProxy - Path is a required Query Argument that is missing."}
 
-    try: return get(f'https://{subdomain}.roblox.com/{path}').json()
+    try: return get(f'https://{subdomain}.roblox.com/{e}').json()
     except JSONDecodeError: return {"success": False, "message": "LegoProxy - Roblox API did not return JSON Data."}
 
 @app.get("/rotate/{subdomain}", description="Rotating Proxy Request")
-async def proxyRequest(subdomain: str, path: str):
+async def proxyRequest(subdomain: str, e: str, password: str = None):
     if subdomain == None: return {"success": False, "message": "LegoProxy - Subdomain is a required Path Argument that is missing."}
-    if path == None: return {"success": False, "message": "LegoProxy - Path is a required Query Argument that is missing."}
+    if e == None: return {"success": False, "message": "LegoProxy - Path is a required Query Argument that is missing."}
     
     if proxylist == []: return {"success": False, "message": "LegoProxy - Proxy IP List is Empty. Please add IPs and Restart LegoProxy."}
     proxy = choice(proxylist)
-    try: return get(f'https://{subdomain}.roblox.com/{path}', proxies={"http": f"http://{proxy}"}).json()
+    try: return get(f'https://{subdomain}.roblox.com/{e}', proxies={"http": f"http://{proxy}"}).json()
     except JSONDecodeError: return {"success": False, "message": "LegoProxy - Roblox API did not return JSON Data."}
     except ConnectTimeout: return {"success": False, "message": f"LegoProxy - Proxy Timed Out. Proxy IP: {proxy}"}
