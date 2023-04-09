308229d85daea087768bc93ac31eb7adb12e3e8f
PyTsun <53323309+PyTsun@users.noreply.github.com>
2023-04-09 13:29:53 +0800

    Delete core directory

--- core/auth/dashboard.py
+++
@@ -1,6 +0,0 @@
-from core.conf import LegoProxyConfig
-
-def validate_credentials(username: str, password: str, config: LegoProxyConfig):
-    if username != config.dashboardUsername: return False
-    if password != config.dashboardPassword: return False
-    return True
